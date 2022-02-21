1339f52d3f688c28c5f6f68ae9cc1392066e52ae
Sam <programming@vectors.net.au>
2022-02-21 20:43:31 +0800

    header calcs

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,7 +1,9 @@
 import { Component, Inject, Input, OnInit, Renderer2 } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { DOCUMENT } from '@angular/common';
-import { BreakpointObserver } from '@angular/cdk/layout';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -12,6 +14,13 @@ export class HeaderComponent implements OnInit {
   readonly themeAnchor = this.document.getElementById('app-theme');
   currentTheme: 'light-theme' | 'dark-theme' = 'light-theme';
   @Input() inputSideNav!: MatSidenav;
+
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map((results) => results.matches),
+      shareReplay()
+    );
 
   constructor(
     private breakpointObserver: BreakpointObserver,
