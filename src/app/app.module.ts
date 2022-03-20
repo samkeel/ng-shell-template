45f999fe343a1a1bd8345b1c0a172b9ed41a03b1
Sam Keel <programming@vectors.net.au>
2022-03-20 21:24:41 +0800

    fxflex added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { SharedModule } from './shared/shared.module';
 import { SnackbarService } from './shared/services/snackbar.service';
 
@@ -15,7 +16,8 @@ import { SnackbarService } from './shared/services/snackbar.service';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    FlexLayoutModule
   ],
   providers: [SnackbarService],
   bootstrap: [AppComponent]
